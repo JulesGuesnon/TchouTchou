1898e1c9fba8d3f6baea4e76f6170feb55299377
Jules Guesnon <jules.guesnon@hetic.net>
2020-01-11 16:13:58 +0100

    :sparkles: Made the basics of the inventory

---
+++ app/src/main/java/com/example/tchoutchou/logic/Inventory.kt
@@ -0,0 +1,47 @@
+package com.example.tchoutchou.logic
+
+class Inventory (val size: Int) {
+    private val slots = SlotManager<Item>(size)
+
+
+    fun add(item: Item): Boolean {
+        if (slots.hasFreeSlot()) {
+            slots.set(slots.getFreeSlot(), item)
+
+            return true
+        }
+
+        return false
+    }
+
+    fun remove(item: Item): Boolean {
+        return slots.delete(item)
+    }
+
+    fun replace(itemToRemove: Item, itemToAdd: Item): Boolean {
+        if (!slots.has(itemToRemove)) {
+            return false
+        }
+
+        if (slots.has(itemToAdd)) {
+            slots.delete(itemToAdd)
+
+        }
+
+        slots.set(
+            slots.getSlotNumber(itemToRemove),
+            itemToAdd
+        )
+
+        return true
+    }
+
+    fun print() {
+        for (i in 0 until size) {
+            println("-----------${i}-----------")
+            println("Name: " + slots.get(i)?.name)
+            println("Descritpion: " + slots.get(i)?.description)
+            println("\n")
+        }
+    }
+}
